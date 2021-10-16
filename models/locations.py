0bbcf2eb901ccf97e3598b97544caaea86013af0
lemejiamo <luismejia69@gmail.com>
2021-10-16 02:27:29 -0500

    locations model

---
+++ models/locations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Model to Locations table"""
+
+
+import models
+from models.base_model import IOT_Model, Base
+from sqlalchemy.sql.sqltypes import DateTime
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+
+
+class Location(IOT_Model, Base):
+    """Representation of a Location"""
+    
+    __tablename__ = 'locations'
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String(30), nullable=False)
+    devices = relationship('Device',
+                            backref='locations')
+    
+    def __init__(self, **kwargs):
+        """initializes location"""
+        super().__init__(**kwargs)
+
